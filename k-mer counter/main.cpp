5d74853fd4389924b6fc59eda16ede44a640be79
Daniel Dalquen <ddalquen@me.com>
2016-03-23 21:31:15 +0000

    Initial Commit

---
+++ k-mer counter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  k-mer counter
+//
+//  Created by Daniel Dalquen on 23/03/2016.
+//  Copyright © 2016 Daniel Dalquen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
